022f1422fef190b21d864606cb9c97ddb38a9bcc
Михаил Пилипенко <pilipenkohit433@gmail.com>
2024-03-01 15:16:17 +0800

    add bootstrap

--- resources/views/insert.blade.php
+++ resources/views/insert.blade.php
@@ -1,5 +1,8 @@
 @extends('welcome')
 
 @section('content')
-ДА
+<form method="POST" action="/profile">
+    @csrf
+    <button type="button" class="btn btn-primary ">Block level button</button>
+    </form>
 @endsection
