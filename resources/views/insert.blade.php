39cce94cafeb3d9fb703909840f27464b933cbfc
Михаил Пилипенко <pilipenkohit433@gmail.com>
2024-03-01 15:18:24 +0800

    fix

--- resources/views/insert.blade.php
+++ resources/views/insert.blade.php
@@ -1,7 +1,7 @@
 @extends('welcome')
 
 @section('content')
-<form method="POST" action="/profile">
+<form method="POST" action="/">
     @csrf
     <button type="button" class="btn btn-primary ">Block level button</button>
     </form>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return Insertcontroller::get();
+    Insertcontroller::get();
 });
 
 Route::post('/', function (Request $request) {
