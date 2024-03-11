6570f0ef127e5c4a67c1f6629fe886012c7311b9
Михаил Пилипенко <pilipenkohit433@gmail.com>
2024-03-11 11:35:00 +0800

    try campus auth half-final

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
 
-    public static function campus_auth()
+    public static function campus_auth($request)
     {
         # переменные
         $return = false;
@@ -19,13 +19,15 @@ class AuthController extends Controller
         # ЭТАП 1 - авторизация учетной записи в ЛИЧНОМ КАБИНЕТЕ
         # редирект на страницу авторизации
         # редирект обратно после успешной авторизации
-        if (!isset($_REQUEST['code'])) {
+        // if (!isset($_REQUEST['code'])) {
+        if (!$request->get('code')) {
             header('HTTP 302 Found');
-            header('Location: https://int.istu.edu/oauth/authorize/?client_id=' . $APP['ID']);
+            return header('Location: https://int.istu.edu/oauth/authorize/?client_id=' . $APP['ID']);
             exit;
         }
         # ЭТАП 2 - авторизация приложения
-        if (isset($_REQUEST['code'])) {
+        $client = new \GuzzleHttp\Client();
+        if ($request->get('code')) {
             # формирование параметров запроса
             $url = implode('&', [
                 'https://int.istu.edu/oauth/token/?grant_type=authorization_code',
@@ -35,25 +37,30 @@ class AuthController extends Controller
             ]);
 
             # выполнение запроса и обработка ответа
+            $res = $client->get($url);
+            $data = (string) $res->getBody();
+            //$data = @file_get_contents($url);
 
-            $data = @file_get_contents($url);
+            //if (explode(' ', $http_response_header[0])[1] !== '200') return false;
+            $data = json_decode($data, true);
+        }
+        # ЭТАП 3 - запрос данных по учетной записи
+        if (isset($data['client_endpoint']) && isset($data['access_token'])) {
+            # формирование параметров запроса
+            $url = $data['client_endpoint'] . 'user.info.json?auth=' . $data['access_token'];
+
+            # выполнение запроса и обработка ответа
+            $res = $client->get($url);
+            $data = (string) $res->getBody();
+            //$data = @file_get_contents($url);
 
             if (explode(' ', $http_response_header[0])[1] !== '200') return false;
             $data = json_decode($data, true);
+            # проверка наличия структуры данных
+            if (isset($data['result']['email'])) $return = $data['result'];
         }
-        // # ЭТАП 3 - запрос данных по учетной записи
-        // if (isset($data['client_endpoint']) && isset($data['access_token'])) {
-        //     # формирование параметров запроса
-        //     $url = $data['client_endpoint'] . 'user.info.json?auth=' . $data['access_token'];
-        //     # выполнение запроса и обработка ответа
-        //     $data = @file_get_contents($url);
-        //     if (explode(' ', $http_response_header[0])[1] !== '200') return false;
-        //     $data = json_decode($data, true);
-        //     # проверка наличия структуры данных
-        //     if (isset($data['result']['email'])) $return = $data['result'];
-        // }
-        // # возврат
-        // return $return;
-        dd($data);
+        # возврат
+        //return $return;
+        dd($return);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,57 +20,59 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     //return Insertcontroller::get();
     // return AuthController::campus_auth();
-    $APP = [
-        'ID' => 'local.65e57544bd23d6.71638042',
-        'CODE' => 'RtRYlevERpBl4jC4AjeOO8U8sivYstMpzwGsA954fId8OCtdWR'
-    ];
-    return header('Location: https://int.istu.edu/oauth/authorize/?client_id=' . $APP['ID']);
+    // $APP = [
+    //     'ID' => 'local.65e57544bd23d6.71638042',
+    //     'CODE' => 'RtRYlevERpBl4jC4AjeOO8U8sivYstMpzwGsA954fId8OCtdWR'
+    // ];
+    // return header('Location: https://int.istu.edu/oauth/authorize/?client_id=' . $APP['ID']);
+    return header('Location: /bitrix');
 });
 
 Route::get('/bitrix', function (Request $request) {
-    // dd($request);
-    $APP = [
-        'ID' => 'local.65e57544bd23d6.71638042',
-        'CODE' => 'RtRYlevERpBl4jC4AjeOO8U8sivYstMpzwGsA954fId8OCtdWR'
-    ];
-    $client = new \GuzzleHttp\Client();
-    if (isset($_REQUEST['code'])) {
+    return AuthController::campus_auth($request);
+    // // dd($request);
+    // $APP = [
+    //     'ID' => 'local.65e57544bd23d6.71638042',
+    //     'CODE' => 'RtRYlevERpBl4jC4AjeOO8U8sivYstMpzwGsA954fId8OCtdWR'
+    // ];
+    // $client = new \GuzzleHttp\Client();
+    // if (isset($_REQUEST['code'])) {
         
-        # формирование параметров запроса
-        $url = implode('&', [
-            'https://int.istu.edu/oauth/token/?grant_type=authorization_code',
-            'code=' . $_REQUEST['code'],
-            'client_id=' . $APP['ID'],
-            'client_secret=' . $APP['CODE']
-        ]);
+    //     # формирование параметров запроса
+    //     $url = implode('&', [
+    //         'https://int.istu.edu/oauth/token/?grant_type=authorization_code',
+    //         'code=' . $_REQUEST['code'],
+    //         'client_id=' . $APP['ID'],
+    //         'client_secret=' . $APP['CODE']
+    //     ]);
 
-        # выполнение запроса и обработка ответа
-        $res = $client->get($url);
-        $data = (string) $res->getBody();
+    //     # выполнение запроса и обработка ответа
+    //     $res = $client->get($url);
+    //     $data = (string) $res->getBody();
         
-        //dd($content);
+    //     //dd($content);
 
-        //$data = @file_get_contents($url);
+    //     //$data = @file_get_contents($url);
 
-        //if (explode(' ', $http_response_header[0])[1] !== '200') return false;
-        $data = json_decode($data, true);
+    //     //if (explode(' ', $http_response_header[0])[1] !== '200') return false;
+    //     $data = json_decode($data, true);
         
-        //dd($data);
-    }
-    if (isset($data['client_endpoint']) && isset($data['access_token'])) {
-        # формирование параметров запроса
-        $url = $data['client_endpoint'] . 'user.info.json?auth=' . $data['access_token'];
+    //     //dd($data);
+    // }
+    // if (isset($data['client_endpoint']) && isset($data['access_token'])) {
+    //     # формирование параметров запроса
+    //     $url = $data['client_endpoint'] . 'user.info.json?auth=' . $data['access_token'];
 
-        # выполнение запроса и обработка ответа
-        //$data = @file_get_contents($url);
-        $res = $client->get($url);
-        $data = (string) $res->getBody();
-        //dd($data);
+    //     # выполнение запроса и обработка ответа
+    //     //$data = @file_get_contents($url);
+    //     $res = $client->get($url);
+    //     $data = (string) $res->getBody();
+    //     //dd($data);
          
-        //if (explode(' ', $http_response_header[0])[1] !== '200') return false;
-        $data = json_decode($data, true);
-        dd($data);
-        # проверка наличия структуры данных
-        if (isset($data['result']['email'])) $return = $data['result'];
-    }
+    //     //if (explode(' ', $http_response_header[0])[1] !== '200') return false;
+    //     $data = json_decode($data, true);
+    //     dd($data);
+    //     # проверка наличия структуры данных
+    //     if (isset($data['result']['email'])) $return = $data['result'];
+    // }
 });
