f63c3a7b1615b4d5f96e423200260ea33e1e760c
Михаил Пилипенко <pilipenkohit433@gmail.com>
2024-03-11 11:21:30 +0800

    try campus auth

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::get('/bitrix', function (Request $request) {
         'ID' => 'local.65e57544bd23d6.71638042',
         'CODE' => 'RtRYlevERpBl4jC4AjeOO8U8sivYstMpzwGsA954fId8OCtdWR'
     ];
+    $client = new \GuzzleHttp\Client();
     if (isset($_REQUEST['code'])) {
         
         # формирование параметров запроса
@@ -44,18 +45,31 @@ Route::get('/bitrix', function (Request $request) {
         ]);
 
         # выполнение запроса и обработка ответа
-
-        $client = new \GuzzleHttp\Client();
         $res = $client->get($url);
-        $content = (string) $res->getBody();
+        $data = (string) $res->getBody();
         
-        dd($content);
+        //dd($content);
 
-        $data = @file_get_contents($url);
+        //$data = @file_get_contents($url);
 
-        if (explode(' ', $http_response_header[0])[1] !== '200') return false;
+        //if (explode(' ', $http_response_header[0])[1] !== '200') return false;
         $data = json_decode($data, true);
         
         dd($data);
+    }
+    if (isset($data['client_endpoint']) && isset($data['access_token'])) {
+        # формирование параметров запроса
+        $url = $data['client_endpoint'] . 'user.info.json?auth=' . $data['access_token'];
+
+        # выполнение запроса и обработка ответа
+        //$data = @file_get_contents($url);
+        $res = $client->get($url);
+        $data = (string) $res->getBody();
+        dd($data);
+         
+        if (explode(' ', $http_response_header[0])[1] !== '200') return false;
+        $data = json_decode($data, true);
+        # проверка наличия структуры данных
+        if (isset($data['result']['email'])) $return = $data['result'];
     }
 });
