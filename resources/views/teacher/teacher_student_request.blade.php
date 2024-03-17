d871a060c2a66b9941e3cd9f010090c1d0052943
Михаил Пилипенко <pilipenkohit433@gmail.com>
2024-03-17 18:23:38 +0800

    table var.2

--- resources/views/teacher/teacher_student_request.blade.php
+++ resources/views/teacher/teacher_student_request.blade.php
@@ -1,27 +1,91 @@
 @extends('layouts.app')
 @section('content')
     <table class="table">
-        <tr class="tr">
-            <th class="th_1">ФИО</th>
-            <th class="th_2">Компания</th>
-            <th class="th_3">Тема</th>
-            <th class="th_4">Статус</th>
-        </tr>
-        @foreach ($student_practics as $stud_prac)
+        <div class="remote">
+
+            <div class="remote-left">
+                <p class="mar-off">Осталось мест: {{ $teacher->work_load }}</p>
+            </div>
+
+            <div class="remote-rigth">
+                <p class="mar-off">Показать отмененные</p>
+                <form method="post">
+                    <label class="label-check">
+                        Тут будет чекбокс просмотреть уже принятых
+                    </label>
+                </form>
+            </div>
+        </div>
+        <thead class="thead">
             <tr class="tr">
-                <td class="td"> {{ $stud_prac->student->fio }} </td>
-                <td class="td"> {{ $stud_prac->company->name }} </td>
-                <td class="td">
-                    <input name="new_theme" type="text" class="theme_text_box" value={{ $stud_prac->theme }}>
-                    <button name="student_theme" class="btn_change" value={{ $stud_prac->id }} type="submit">Принять
-                        изменения</button>А
-                </td>
-                <td class="td">
-                    <button name="student_id" class="btn" value={{ $stud_prac->id }} type="submit">Принять
-                        заявку</button>
-                </td>
+                <th class="th">ФИО студента</th>
+                <th class="th">Компания</th>
+                <th class="th">Тема практики</th>
+                <th class="th">Договор</th>
+                <th class="th">Статус</th>
+                <th class="th">Действие</th>
             </tr>
-        @endforeach
+        </thead>
+        <tbody class="tbody">
+            @foreach ($student_practics as $stud_prac)
+                <tr class="tr">
+
+                    <td class="td">
+                        <div class="block-div">
+                            <strong class="strong">{{ $stud_prac->student->fio }} </strong>
+                        </div>
+                    </td>
+
+                    @if ($stud_prac->company->name)
+                        <td class="td">
+                            <div class="block-div"> {{ $stud_prac->company->name }} </div>
+                        </td>
+                    @else
+                        <td class="td">
+                            <div class="block-div">
+                                <p class="mar-off">Своя компания</p>
+                            </div>
+                        </td>
+                    @endif
+
+
+                    <td class="td">
+                        <div class="block-div"> {{ $stud_prac->theme }} </div">
+                    </td>
+
+                    <td class="td">
+                        <button name="download" value={{ $stud_prac->student->id }} class="btn">Файл</button>
+                    </td>
+
+                    @if ($stud_prac->status)
+                        <td class="td td-status">
+                            <div class="block-status_ok">
+                                <span class="status-check_ok">Принят</span>
+                            </div>
+                        </td>
+                    @else
+                        <td class="td td-status">
+                            <div class="block-status_fail">
+                                <span class="status-check_fail">Не принят</span>
+                            </div>
+                        </td>
+                    @endif
+
+                    <td class="td">
+                        <ul class="action" aria-labelledby="btnGroupDrop1">
+                            <form>
+                                <li><button type="sumbit" name="done" value={{ $stud_prac->id }}
+                                        class="btn dropdown-item1" href="#"></button></li>
+                            </form>
+                            <form>
+                                <li><button type="sumbit" name="remake" value={{ $stud_prac->id }}
+                                        class="btn dropdown-item2" href="#"></button></li>
+                            </form>
+                        </ul>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
     </table>
 @endsection
 
