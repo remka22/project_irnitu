a668209da3633539c9ad869f7bd7578e31500138
Михаил Пилипенко <pilipenkohit433@gmail.com>
2024-03-17 19:14:50 +0800

    fix

--- app/Http/Controllers/Teacher/TeacherSetudentRequest.php
+++ app/Http/Controllers/Teacher/TeacherSetudentRequest.php
@@ -51,7 +51,7 @@ function cancel_request_practic($request)
     $user = Auth::user();
     $teacher = Teachers::where('mira_id', $user->mira_id)->get()->first();
     increase_work_load($teacher->id);
-    $stud_prac = StudentPractic::find($request->input('cancel'));
+    $stud_prac = StudentPractic::find($request->input('remake'));
     $stud_prac->update(['status' => 2]);
     $stud_prac->save();
 }
