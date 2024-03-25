09335083af3d21f4f786da511220c8a388195dce
Михаил Пилипенко <pilipenkohit433@gmail.com>
2024-03-25 20:34:11 +0800

    fix

--- app/Http/Controllers/Direct/DirectController.php
+++ app/Http/Controllers/Direct/DirectController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Direct;
 
 use App\Http\Controllers\Controller;
+use App\Models\Group;
 use App\Models\GroupScore;
+use App\Models\Stream;
+use App\Models\Teachers;
 use App\Models\TeacherScore;
 use Illuminate\Http\Request;
 
@@ -11,11 +14,12 @@ class DirectController extends Controller
 {
     public static function get()
     {
-        $group = "ИСТб-20-1";
-        $teacher = " Аршинский В.Л.";
-        $teachers = GroupScore::where('group_id', $group)->with('teachers')->get();
+        $stream = Stream::where('name', "ИСТб-20")->get()->first();
+        $group = Group::where([['stream_id', '=', $stream->id], ['group_number', '=', 1]])->get()->first();
+        $teacher = Teachers::where('fio','like', "%Аршинский%")->get()->first();
+        $teachers = GroupScore::where('group_id', $group->id)->with('teachers')->get();
         dump($teachers);
-        $groups = TeacherScore::where('teacher_id', $teacher)->with('groups')->get();
+        $groups = TeacherScore::where('teacher_id', $teacher->id)->with('groups')->get();
         dd($groups);
         return view('direct.direct');
     }
