3ca791a1248a62dfcd1bf6bda5244361bf2b08fc
Михаил Пилипенко <pilipenkohit433@gmail.com>
2024-04-08 20:58:39 +0800

    fix cancel stud report from teacher

--- app/Http/Controllers/Teacher/TeacherSetudentRequest.php
+++ app/Http/Controllers/Teacher/TeacherSetudentRequest.php
@@ -17,22 +17,21 @@ class TeacherSetudentRequest extends Controller
         $user = Auth::user();
         $teacher = Teachers::where('mira_id', $user->mira_id)->get()->first();
         $t_score = TeacherScore::where('teacher_id', '=', $teacher->id)->get();
-        
+
         $arr_id_tscore = [];
         $work_load = 0;
-        foreach ($t_score as $ts){
+        foreach ($t_score as $ts) {
             $arr_id_tscore[] = $ts->id;
             $work_load += $ts->score;
         }
 
-        if($request->get('check')){
+        if ($request->get('check')) {
             $student_practics = StudentPractic::whereIn('teacher_id', $arr_id_tscore)->with('company', 'student.group.stream')->get();
-        }
-        else{
+        } else {
             $student_practics = StudentPractic::where('status', "<>", 2)->whereIn('teacher_id', $arr_id_tscore)->with('company', 'student.group.stream')->get();
         }
 
-        
+
         return view('teacher.teacher_student_request', [
             'student_practics' => $student_practics,
             'teacher' => $teacher,
@@ -47,7 +46,7 @@ class TeacherSetudentRequest extends Controller
             done_request_practic($request);
         } elseif ($request->input('remake')) {
             cancel_request_practic($request);
-        }elseif ($request->input('download')) {
+        } elseif ($request->input('download')) {
             return Storage::download($request->input('download'));
         }
         return redirect('/teacher/stud_practika');
@@ -60,28 +59,34 @@ function done_request_practic($request)
     $teacher = Teachers::where('mira_id', $user->mira_id)->get()->first();
     $t_score = TeacherScore::where('teacher_id', $teacher->id)->get();
     $arr_id_tscore = [];
-    foreach ($t_score as $ts){
+    foreach ($t_score as $ts) {
         $arr_id_tscore[] = $ts->id;
     }
     $stud_prac = StudentPractic::whereIn('teacher_id', $arr_id_tscore)->where('id', '=', $request->input('done'))->get()->first();
-    if($stud_prac){
-        if($stud_prac->status == 2){
+    if ($stud_prac) {
+        if ($stud_prac->status == 2) {
             decriment_work_load($stud_prac->teacher_id);
-        }   
+        }
         $stud_prac->update(['status' => 1]);
         $stud_prac->save();
-    }    
+    }
 }
 
 function cancel_request_practic($request)
 {
     $user = Auth::user();
     $teacher = Teachers::where('mira_id', $user->mira_id)->get()->first();
-    $stud_prac = StudentPractic::where([['id', '=', $request->input('remake')], ['teacher_id', '=', $teacher->id]])->get()->first();
-    increase_work_load($stud_prac->teacher_id);
-    $stud_prac->update(['status' => 2]);
-    $stud_prac->save();
-    
+    $t_score = TeacherScore::where('teacher_id', $teacher->id)->get();
+    $arr_id_tscore = [];
+    foreach ($t_score as $ts) {
+        $arr_id_tscore[] = $ts->id;
+    }
+    $stud_prac = StudentPractic::whereIn('teacher_id', $arr_id_tscore)->where([['id', '=', $request->input('remake')], ['teacher_id', '=', $teacher->id]])->get()->first();
+    if ($stud_prac) {
+        increase_work_load($stud_prac->teacher_id);
+        $stud_prac->update(['status' => 2]);
+        $stud_prac->save();
+    }
 }
 
 function increase_work_load($teacher_id)
