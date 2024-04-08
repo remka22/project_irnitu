b64c0bc5655e7c7e209da2b33cc7993135848c35
Михаил Пилипенко <pilipenkohit433@gmail.com>
2024-04-08 20:55:19 +0800

    fix

--- app/Http/Controllers/Student/StudentReport.php
+++ app/Http/Controllers/Student/StudentReport.php
@@ -147,8 +147,7 @@ function increase_work_load($teacher_id)
 
 function work_load_check($teacher_id, $group_id)
 {
-    $grp_score = GroupScore::where([['teacher_score_id', '=', $teacher_id], ['group_id', '=', $group_id]])->get();
-    dd($grp_score);
+    $grp_score = GroupScore::where([['teacher_score_id', '=', $teacher_id], ['group_id', '=', $group_id]])->get()->first();
     if ($grp_score) {
         $work_load = TeacherScore::find($teacher_id)->get()->first()->score;
         if ($work_load > 0) {
