0c5e0d9693903600e16856886ca96b1a551622d3
Михаил Пилипенко <pilipenkohit433@gmail.com>
2024-04-09 11:47:55 +0800

    fix

--- resources/views/student/student_otchet_ban.blade.php
+++ resources/views/student/student_otchet_ban.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row">
+<div class="container d-flex">
+    <div class="row justify-content-center">
       <div class="col align-self-center">
         <div class="alert alert-warning" role="alert">
             Ваша заявка не была принята преподавателем, или время еще не пришло
