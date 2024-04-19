c33016ee40bff93145cb735ea2ab4aed959a1889
Михаил Пилипенко <pilipenkohit433@gmail.com>
2024-04-19 14:20:38 +0800

    fix

--- resources/views/direct/direct.blade.php
+++ resources/views/direct/direct.blade.php
@@ -11,6 +11,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Загрузка нагрузки преподавателей</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 @if ($workload)
                 <div class="modal-body">
