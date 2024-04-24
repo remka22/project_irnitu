b00a28610f638f206718b159e8db5eca85f67ebd
Михаил Пилипенко <pilipenkohit433@gmail.com>
2024-04-24 12:46:57 +0800

    fix

--- app/Http/Controllers/Direct/DirectController.php
+++ app/Http/Controllers/Direct/DirectController.php
@@ -73,7 +73,7 @@ class DirectController extends Controller
                 $templatesModel->save();
             }
 
-            self::index();
+            redirect('/direct/shablon_prikazy');
         }
     }
 }
